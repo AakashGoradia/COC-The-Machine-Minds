7d423f667562ec8ee12c2f5f1c92f8429c446f21
AryanGupta2406 <aryanworkgp2406@gmail.com>
2023-03-04 22:30:29 +0530

    contact&sub commit

--- demo/src/components/Subscribe.js
+++ demo/src/components/Subscribe.js
@@ -1,4 +1,68 @@
+// import React from 'react';
+
+// function SubscriptionPage() {
+//   return (
+//     <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+//       <h1 className="text-3xl font-bold mb-4">Subscribe Now!</h1>
+//       <div className="grid grid-cols-3 gap-4">
+//         <div className="bg-white rounded-lg shadow-lg p-8 w-full">
+//           <h2 className="text-xl font-bold mb-4">Basic Plan</h2>
+//           <p className="text-lg mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dictum ante, quis varius felis.</p>
+//           <ul className="list-disc list-inside mb-4">
+//             <li>Feature 1</li>
+//             <li>Feature 2</li>
+//             <li>Feature 3</li>
+//           </ul>
+//           <div className="flex justify-center">
+//             <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+//               Select Plan
+//             </button>
+//           </div>
+//         </div>
+//         <div className="bg-white rounded-lg shadow-lg p-8 w-full">
+//           <h2 className="text-xl font-bold mb-4">Pro Plan</h2>
+//           <p className="text-lg mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dictum ante, quis varius felis.</p>
+//           <ul className="list-disc list-inside mb-4">
+//             <li>Feature 1</li>
+//             <li>Feature 2</li>
+//             <li>Feature 3</li>
+//             <li>Feature 4</li>
+//             <li>Feature 5</li>
+//           </ul>
+//           <div className="flex justify-center">
+//             <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+//               Select Plan
+//             </button>
+//           </div>
+//         </div>
+//         <div className="bg-white rounded-lg shadow-lg p-8 w-full">
+//           <h2 className="text-xl font-bold mb-4">Premium Plan</h2>
+//           <p className="text-lg mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at dictum ante, quis varius felis.</p>
+//           <ul className="list-disc list-inside mb-4">
+//             <li>Feature 1</li>
+//             <li>Feature 2</li>
+//             <li>Feature 3</li>
+//             <li>Feature 4</li>
+//             <li>Feature 5</li>
+//             <li>Feature 6</li>
+//             <li>Feature 7</li>
+//           </ul>
+//           <div className="flex justify-center">
+//             <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500">
+//               Select Plan
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SubscriptionPage;
+
 import React from 'react';
+import rect from "../assets/Rectangle 25.svg"
+import vector from "../assets/Arrow.svg"
 
 function SubscriptionPage() {
   return (
@@ -38,16 +102,61 @@ function SubscriptionPage() {
     //   </div>
     // </div>
     <div className='bg-gradient-to-b from-[#cee3fc] to-[#edf5fe]'>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 lg:gap-3 justify-items-center mt-10'>
+            <p className='text-[#C71A03] text-[40px] font-["Montserrat"] font-bold text-center'>Subscription Plans</p>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 lg:gap-3 justify-items-center mt-10'>
                 <div className="py-10 w-72 ">
-                    <div className="rounded-lg overflow-hidden shadow-lg shadow-gray-600 max-w-sm bg-white">
+                    <div className="rounded-lg overflow-hidden shadow-lg shadow-gray-600 max-w-sm bg-white lg:h-148">
                         <img src={rect} alt="" className='w-full px-2' />
                         <div className="px-6 py-4">
                             <div className=''>
-                                PROJECT_1
+                                <strong>Basic</strong>
                             </div>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aspernatur minus optio, eius officia mollitia quod eveniet at atque expedita!
+                                <ul style={{ listStyleType: 'decimal' }}>
+                                  <li>
+                                    Price: $10/month
+                                  </li>
+                                  <li>
+                                    Human presence detection for up to 100 images per month
+                                  </li>
+                                  <li>
+                                    24/7 email support
+                                  </li>
+                                </ul>
+                            </p>
+                            <div className='pt-3 pb-3 flex'>
+                                <span className='text-[#2566FF] p-2'>
+                                    <a href='#'>Know More</a>
+                                </span>
+                                <img src={vector} className="" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="py-10 w-72 ">
+                    <div className="rounded-lg overflow-hidden shadow-lg shadow-gray-600 max-w-sm  bg-white lg:h-140">
+                        <img src={rect} alt="" className='w-full px-2' />
+                        <div className="px-6 py-4">
+                            <div>
+                            <strong>Pro</strong>
+                            </div>
+                            <p>
+                            <ul style={{ listStyleType: 'decimal' }}>
+                                  <li>
+                                    Price: $25/month
+                                  </li>
+                                  <li>
+                                    Human presence detection for up to 500 images per month
+                                  </li>
+                                  <li>
+                                    Priority email support
+                                  </li>
+                                  <li>
+                                    API usage analytics
+                                  </li>
+                                </ul>
                             </p>
                             <div className='pt-3 pb-3 flex'>
                                 <span className='text-[#2566FF] p-2'>
@@ -64,30 +173,25 @@ function SubscriptionPage() {
                         <img src={rect} alt="" className='w-full px-2' />
                         <div className="px-6 py-4">
                             <div>
-                                PROJECT_1
+                                <strong>Ace</strong>
                             </div>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aspernatur minus optio, eius officia mollitia quod eveniet at atque expedita!
-                            </p>
-                            <div className='pt-3 pb-3 flex'>
-                                <span className='text-[#2566FF] p-2'>
-                                    <a href='#'>Know More</a>
-                                </span>
-                                <img src={vector} className="" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="py-10 w-72 ">
-                    <div className="rounded-lg overflow-hidden shadow-lg shadow-gray-600 max-w-sm  bg-white">
-                        <img src={rect} alt="" className='w-full px-2' />
-                        <div className="px-6 py-4">
-                            <div>
-                                PROJECT_1
-                            </div>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aspernatur minus optio, eius officia mollitia quod eveniet at atque expedita!
+                            <ul style={{ listStyleType: 'decimal' }}>
+                                  <li>
+                                    Price: $50/month
+                                  </li>
+                                  <li>
+                                    Includes access to perks from previous levels + 500 images/month
+                                  </li>
+                                  <li>
+                                    Access to documentation and sample code
+                                  </li>
+{/*                                   
+                                  <li>
+                                    Custom integration support
+                                  </li> */}
+                                </ul>
+                                
                             </p>
                             <div className='pt-3 pb-3 flex'>
                                 <span className='text-[#2566FF] p-2'>
@@ -104,10 +208,26 @@ function SubscriptionPage() {
                         <img src={rect} alt="" className='w-full px-2' />
                         <div className="px-6 py-4">
                             <div>
-                                PROJECT_1
+                                <strong>
+                                  Enterprise
+                                </strong>
                             </div>
                             <p >
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo aspernatur minus optio, eius officia mollitia quod eveniet at atque expedita!
+                            <ul style={{ listStyleType: 'decimal' }}>
+                                  <li>
+                                    Price:Custom Pricing based on usage and requirements.
+                                  </li>
+                                  <li>
+                                  Includes access to perks from previous levels
+                                  </li>
+                                  <li>
+                                    Dedicated account manager.
+                                  </li>
+                                  <li>
+                                    Priority Email and Phone support
+                                  </li>
+                                 
+                                </ul>
                             </p>
                             <div className='pt-3 pb-3 flex'>
                                 <span className='text-[#2566FF] p-2'>
@@ -121,7 +241,15 @@ function SubscriptionPage() {
 
 
             </div>
-    </div>
+
+            <div className='mt-5 pb-5'>
+                <p className='text-[24px] text-[#7C7C7C] text-center '>
+                    Authoritatively underwhelm excellent methodologies via premium expertise competitive<br /> than open-source imperatives disseminate.
+                </p>
+            </div>
+
+
+      </div>
   );
 }
 
